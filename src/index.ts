5497a619a73c3533b67c60c8110c9998c6c982bc
panuwat <promnigorn@gmail.com>
2021-06-08 20:26:54 +0700

    Create EKS Cluster & Deploy NGINX

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,136 @@
+import * as awsx from "@pulumi/awsx";
+import * as eks from "@pulumi/eks";
+import * as k8s from "@pulumi/kubernetes";
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+const name = "helloworld";
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+/**
+ * Per NodeGroup IAM: each NodeGroup will bring its own, specific instance role and profile.
+ */
+
+ const managedPolicyArns: string[] = [
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+];
+
+// Creates a role and attaches the EKS worker node IAM managed policies. Used a few times below,
+// to create multiple roles, so we use a function to avoid repeating ourselves.
+export function createRole(name: string): aws.iam.Role {
+    const role = new aws.iam.Role(name, {
+        assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({
+            Service: "ec2.amazonaws.com",
+        }),
+    });
+
+    let counter = 0;
+    for (const policy of managedPolicyArns) {
+        // Create RolePolicyAttachment without returning it.
+        const rpa = new aws.iam.RolePolicyAttachment(`${name}-policy-${counter++}`,
+            { policyArn: policy, role: role },
+        );
+    }
+
+    return role;
+}
+
+// Now create the roles and instance profiles for the two worker groups.
+const role1 = createRole("my-worker-role1");
+const instanceProfile1 = new aws.iam.InstanceProfile("my-instance-profile1", {role: role1});
+
+
+// Create an EKS cluster with non-default configuration
+const vpc = new awsx.ec2.Vpc("my-vpc", { 
+    cidrBlock: "10.0.0.0/16",
+    numberOfAvailabilityZones: "all",
+    subnets: [
+        {type: "public"},
+        {type: "private"}
+    ],
+    numberOfNatGateways: 1,
+});
+
+const cluster = new eks.Cluster(name, {
+    vpcId: vpc.id,
+    subnetIds: vpc.publicSubnetIds,
+    skipDefaultNodeGroup: true,
+    instanceRoles: [ role1],
+});
+
+// First, create a node group for fixed compute.
+const fixedNodeGroup = cluster.createNodeGroup("my-cluster-ng1", {
+    instanceType: "t2.medium",
+    desiredCapacity: 2,
+    minSize: 1,
+    maxSize: 3,
+    labels: {"ondemand": "true"},
+    instanceProfile: instanceProfile1,
+});
+
+// Export the clusters' kubeconfig.
+export const kubeconfig = cluster.kubeconfig;
+
+// Create a Kubernetes Namespace
+const ns = new k8s.core.v1.Namespace(name, {}, { provider: cluster.provider });
+
+// Export the Namespace name
+export const namespaceName = ns.metadata.name;
+
+// Create a NGINX Deployment
+const appLabels = { appClass: name };
+const deployment = new k8s.apps.v1.Deployment(name,
+    {
+        metadata: {
+            namespace: namespaceName,
+            labels: appLabels,
+        },
+        spec: {
+            replicas: 1,
+            selector: { matchLabels: appLabels },
+            template: {
+                metadata: {
+                    labels: appLabels,
+                },
+                spec: {
+                    containers: [
+                        {
+                            name: name,
+                            image: "nginx:latest",
+                            ports: [{ name: "http", containerPort: 80 }],
+                        },
+                    ],
+                },
+            },
+        },
+    },
+    {
+        provider: cluster.provider,
+    },
+);
+
+// Export the Deployment name
+export const deploymentName = deployment.metadata.name;
+
+// Create a LoadBalancer Service for the NGINX Deployment
+const service = new k8s.core.v1.Service(name,
+    {
+        metadata: {
+            labels: appLabels,
+            namespace: namespaceName,
+        },
+        spec: {
+            type: "LoadBalancer",
+            ports: [{ port: 80, targetPort: "http" }],
+            selector: appLabels,
+        },
+    },
+    {
+        provider: cluster.provider,
+    },
+);
+
+// Export the Service name and public LoadBalancer Endpoint
+export const serviceName = service.metadata.name;
+export const serviceHostname = service.status.loadBalancer.ingress[0].hostname;
